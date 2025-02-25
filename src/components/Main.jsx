fc3b583720273a7a3ca30940aa4965a481101c1d
alejandrobaez <alejandrobaez938@gmail.com>
2025-02-25 15:10:26 -0400

    Añadir componente Main con navegación de imágenes y crear componente Products

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
 export default function Main() {
+  const [currentImage, setCurrentImage] = useState(1);
+  const productImg = (num) => {
+    const img = "/images/image-product-" + num + ".jpg";
+    const thumbnail = "/images/image-product-" + num + "-thumbnail.jpg";
+    return [img, thumbnail];
+  };
+
+  const previousImage = () => {
+    setCurrentImage((prev) => (prev === 1 ? 4 : prev - 1));
+  };
+  const nextImage = () => {
+    setCurrentImage((prev) => (prev === 4 ? 1 : prev + 1));
+  };
+
   return (
-    <main class='md:mt-20 '>
-      <img src='/images/image-product-4.jpg' alt='' />
+    <main className="max-w-screen-lg mx-auto  lg:mt-20">
+      <section className='flex relative'>
+        <img className="aspect-square" src={productImg(currentImage)[0]} alt='' />
+        <button
+          onClick={previousImage}
+          className='absolute md:hidden  cursor-pointer flex justify-center items-center top-[50%] -translate-y-[50%] rounded-full bg-white size-10 left-4'
+        >
+          <img src='/images/icon-previous.svg' className='size-3' alt='' />
+        </button>
+        <button
+          onClick={nextImage}
+          className='absolute md:hidden cursor-pointer flex justify-center items-center top-[50%] -translate-y-[50%] rounded-full bg-white size-10 right-4'
+        >
+          <img src='/images/icon-next.svg' className='size-3' alt='' />
+        </button>
+      </section>
+      <section className="p-4"></section>
     </main>
   );
 }
